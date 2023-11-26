a16943f79b2aa37c9f899793fce96ac86b723786
Ayush-Baliyan-19 <ayush.3316403221@ipu.ac.in>
2023-11-26 22:23:43 +0530

    Divyansh Bharwe teri bund faad dunga mai

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,3 +2,7 @@ const elem = document.getElementById('my-elem');
 elem.onclick = function () {
     console.log(elem);
 }
+elem.addEventListener('click', function () {
+    console.log(elem);
+    alert('Hello world');
+});
